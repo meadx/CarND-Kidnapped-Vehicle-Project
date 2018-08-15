d950d4ad18126273eadb485eee7e69adc2d30b23
meadx <35892868+meadx@users.noreply.github.com>
2018-08-15 11:18:09 +0200

    ParticleFilter::prediction() filled

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -33,7 +33,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 			
 	for (i=0; i<num_particles; i++) {
 		particles[i].id = i;
-		particles[i].x = dist_x(gen);;
+		particles[i].x = dist_x(gen);
 		particles[i].y = dist_y(gen);
 		particles[i].theta = dist_theta(gen);
 		// and all weights to 1.
@@ -45,10 +45,25 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
-	// TODO: Add measurements to each particle and add random Gaussian noise.
-	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
-	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
-	//  http://www.cplusplus.com/reference/random/default_random_engine/
+	// Add measurements to each particle
+	for (i=0; i<num_particles; i++) {
+		particles[i].x = particles[i].x + (velocity/yaw_rate)*(sin(particles[i].theta + yaw_rate*delta_t) - sin(particles[i].theta));
+		particles[i].y = particles[i].y + (velocity/yaw_rate)*(cos(particles[i].theta) - cos(particles[i].theta + yaw_rate*delta_t));
+		particles[i].theta = particles[i].theta + yaw_rate*delta_t;
+				
+		// add random Gaussian noise.
+		// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
+		//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
+		//  http://www.cplusplus.com/reference/random/default_random_engine/
+		normal_distribution<double> dist_x(particles[i].x, std_pos[0]);
+		particles[i].x = dist_x(gen);
+		
+		normal_distribution<double> dist_y(particles[i].y, std_pos[1]);
+		particles[i].y = dist_y(gen);
+		
+		normal_distribution<double> dist_theta(particles[i].theta, std_pos[2]);
+		particles[i].theta = dist_theta(gen);
+	}
 
 }
 
